230ec9d7b34e270599c378e25f965bb0eccd9f20
Denise <dbperry2@asu.edu>
2016-06-06 14:43:27 -0700

    GUI Layout complete

--- logicPuzzle/src/logicpuzzle/LogicPuzzle.java
+++ logicPuzzle/src/logicpuzzle/LogicPuzzle.java
@@ -19,6 +19,8 @@ public class LogicPuzzle
     \***********************************/
     
     /*******************************************\
+    * HINTS:					*
+    *********************************************
     * Brit		Red			*
     * Swede		Dog			*
     * Dane		Tea			*
@@ -37,12 +39,30 @@ public class LogicPuzzle
     \*******************************************/
     
     
+    String[] country = {"German", "Swede", "Dane", "Norwegian", "Brit"};
+    String[] animal = {"horse", "dog", "cat", "bird", "fish"};
+    String[] drink = {"milk", "water", "tea", "coffee", "root beer"};
+    String[] cigar = {"Pall Mall", "Blue Masters", "Prince", "Dunhills", "Blends"};
+    String[] color = {"Red", "Blue", "Green", "Yellow", "White"};
+    String[] num = {"1", "2", "3", "4", "5"};
+    String[][] list = {country, animal, drink, cigar, color, num};
+    private int status = DEFAULT;
+    
     public int getStatus(String type1, String type2)
     {
 	
 	
 	return 0;
     }
+    
+    public void setStatus(String type1, String type2, int clicks)
+    {
+	if(clicks == -1)
+	    
+	    
+    }
+    
+    
     public int[] findIndex(String type)
     {
 	int[] index = {0,0};
@@ -56,12 +76,4 @@ public class LogicPuzzle
 		}
 	return index;
     }
-
-    String[] country = {"German", "Swede", "Dane", "Norwegian", "Brit"};
-    String[] animal = {"horse", "dog", "cat", "bird", "fish"};
-    String[] drink = {"milk", "water", "tea", "coffee", "root beer"};
-    String[] cigar = {"Pall Mall", "Blue Masters", "Prince", "Dunhills", "Blends"};
-    String[] color = {"Red", "Blue", "Green", "Yellow", "White"};
-    String[] num = {"1", "2", "3", "4", "5"};
-    String[][] list = {country, animal, drink, cigar, color, num};
 }
